a6c3ec96598742ae9461eb48c778f11ab53180c3
Sheshanath <35036388+sheshanathkumar@users.noreply.github.com>
2022-08-18 13:18:59 +0530

    added deploy ready code

---
+++ src/main/java/com/sk/testwar/TestWarApplication.java
@@ -0,0 +1,14 @@
+package com.sk.testwar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWarApplication.class, args);
+		System.out.println("hello");
+	}
+
+}

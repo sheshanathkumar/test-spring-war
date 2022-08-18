1c67d739f8dd6259726122110faecdaaa12ce242
Sheshanath <35036388+sheshanathkumar@users.noreply.github.com>
2022-08-18 13:27:32 +0530

    added deploy ready code

--- src/test/java/com/sk/testwar/TestWarApplicationTests.java
+++ src/test/java/com/sk/testwar/TestWarApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sk.testwar;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class TestWarApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.sk.testwar;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class TestWarApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
